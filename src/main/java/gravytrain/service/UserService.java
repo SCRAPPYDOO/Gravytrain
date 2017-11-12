381f4cf702f6cb2076f4b29be661baa5844459e4
LENOVO\SCRAPPYDOO <scrapek69@gmail.com>
2017-11-12 18:34:55 +0000

    added UserService

---
+++ src/main/java/gravytrain/service/UserService.java
@@ -0,0 +1,43 @@
+package gravytrain.service;
+
+import java.util.List;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import gravytrain.controller.UserController;
+import gravytrain.repository.UserRepository;
+import gravytrain.repository.user.User;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	public List<User> getUsers() {
+		return (List<User>) userRepository.findAll();
+	}
+
+	public User getUser(int id) {
+		return userRepository.findOne(id);
+	}
+
+	public User addUser(User user) {
+		return userRepository.save(user);
+	}
+
+	public User updateUser(User user) {
+		return this.addUser(user);
+	}
+
+	public void deleteUser(int id) {
+		userRepository.delete(id);
+
+	}
+
+	public List<User> getUsersBySurname(String surname) {
+		return userRepository.findBySurname(surname);
+	}
+}
